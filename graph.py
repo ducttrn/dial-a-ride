190e75cdbb71433ae2aa81a635e29b5719f392d9
Minh Nguyen <mnguyen6@conncoll.edu>
2022-11-16 15:51:28 -0500

    add random graph generator

--- graph.py
+++ graph.py
@@ -1,4 +1,5 @@
 from typing import Dict
+from random import randrange
 
 
 class Node:
@@ -40,4 +41,21 @@ def construct_graph(node_ids, request_data) -> Graph:
         requests[request_id] = request
 
     graph = Graph(nodes, requests)
+    return graph
+
+    
+def generateRequestsUniform(numberOfNodes: int, numberOfRequests: int) -> Graph:
+    #number of nodes is less than 26
+    node_ids_ = [chr(node + 65) for node in range(numberOfNodes)]
+    request_data = {}
+
+    for i in range(numberOfRequests):
+        source = randrange(numberOfNodes)
+        destination = randrange(numberOfNodes)
+        while destination == source:
+            destination = randrange(numberOfNodes)
+
+        request_data[str(i + 1)] = (node_ids_[source], node_ids_[destination])
+
+    graph = construct_graph(node_ids_, request_data)
     return graph
