0140f729b1a6ead5a9ae364baffe10924be5e277
Minh Nguyen <mnguyen6@conncoll.edu>
2022-11-22 13:44:02 -0500

    fix file bug, build UI

--- find_lcf.py
+++ find_lcf.py
@@ -2,6 +2,7 @@ from find_lc import find_longest_chain, find_longest_chain_no_removals
 from find_opt import optimal2
 from graph import construct_graph, generateRequestsUniform, Graph
 from random import randrange
+import time
 
 
 def find_lcf_outcome(graph: Graph, time_limit: int, no_removals=False) -> int:
@@ -46,22 +47,43 @@ if __name__ == "__main__":
         "6": ("B", "D"),
     }
 
-    for i in range(100):
-        time_limit = randrange(5,15)
-        graph = generateRequestsUniform(numberOfNodes = randrange(4,8), numberOfRequests = randrange(8,13)) # num of nodes needs to > 1
+    count = 0
+    iterations = int(input("How many iterations do you want to run? "))
+    time_limit_min = int(input("Minimum time limit? "))
+    time_limit_max = int(input("Maximum time limit? "))
+    nodes_min = int(input("Minimum number of nodes? "))
+    nodes_max = int(input("Maximum number of nodes? "))
+    requests_min = int(input("Minimum number of requests? "))
+    requests_max = int(input("Maximum number of requests? "))
+    print_to_terminal = int(input("Do you want to print the results to the terminal? 1 for yes and 0 for no: "))
+    for i in range(iterations):
+        start = time.time()
+        time_limit = randrange(time_limit_min, time_limit_max)
+        numberOfNodes = randrange(nodes_min, nodes_max)
+        numberOfRequests = randrange(requests_min, requests_max)
+        graph = generateRequestsUniform(numberOfNodes, numberOfRequests) # num of nodes needs to > 1
 
         opt = optimal2(graph, time_limit)
         lcf = find_lcf_outcome(graph, time_limit)
+        ratio = float(opt/lcf)
 
         print(f"Iteration: {i+1}")
-        print(f"Time limit: {time_limit}")
-        print(f"Optimal: {opt}")
-        print(f"LCF: {lcf}")
-
-        ratio = float(opt/lcf)
+        
+        if print_to_terminal:
+            print(f"Number of nodes: {numberOfNodes}, Number of requests {numberOfRequests}")
+            print(f"Graph Nodes: {[node for node in graph.nodes]}")
+            print(f"Graph Requests: {[(request.id, request.src.id, request.dst.id) for request in graph.requests.values()]}")
+            print(f"Time limit: {time_limit}")
+            print(f"Optimal: {opt}")
+            print(f"LCF: {lcf}")
+            print(f"OPT to LCF ratio: {ratio}")
+            print(f"Time taken: {time.time() - start}")
+        print(f"Ratio greater than 1: {count} times")
+        print(f"---------------------------")
         if ratio < 1:
             raise Exception(f"Wrong ratio")
         elif ratio > 1:
+            count += 1
             f = open("ratio.txt", "a")
             f.write(f"Number of nodes: {numberOfNodes}, Number of requests {numberOfRequests}")
             f.write(f"Graph Nodes: {[node for node in graph.nodes]}")
@@ -73,6 +95,3 @@ if __name__ == "__main__":
             f.write(f"OPT to LCF ratio: {ratio}")
             f.write(f"---------------------------")
             f.close()
-
-        print(f"OPT to LCF ratio: {ratio}")
-        print(f"---------------------------")
