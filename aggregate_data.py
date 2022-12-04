7ccf69b310605616a821e69a2b20ff6cdc85c605
Bill <trantriducs@gmail.com>
2022-12-04 03:07:18 -0500

    scale time_limit + script to aggregate data

---
+++ aggregate_data.py
@@ -0,0 +1,24 @@
+import csv
+
+if __name__ == "__main__":
+    dfs_backtrack_sums = {}
+    dfs_normal_sums = {}
+    instances_count = {}
+
+    with open('simulation-data.csv', 'r') as f:
+        reader = csv.reader(f)
+        data = list(reader)
+        for row in data:
+            dfs_backtrack_sums[row[1]] = dfs_backtrack_sums.get(row[1], 0) + int(row[4])
+            dfs_normal_sums[row[1]] = dfs_normal_sums.get(row[1], 0) + int(row[5])
+            instances_count[row[1]] = instances_count.get(row[1], 0) + 1
+
+    with open('simulation-data-aggregated.csv', 'a') as f:
+        writer = csv.writer(f)
+        writer.writerow(['requests_count', 'dfs_backtrack', 'dfs_normal'])
+        for key in instances_count:
+            writer.writerow([
+                key,
+                dfs_backtrack_sums[key] / instances_count[key],
+                dfs_normal_sums[key] / instances_count[key]
+            ])
--- simulation.py
+++ simulation.py
@@ -41,12 +41,14 @@ if __name__ == "__main__":
         time_limit = randrange(time_limit_min, time_limit_max + 1)
         nodes_count = randrange(nodes_min, nodes_max + 1)
         requests_count = randrange(requests_min, requests_max + 1)
+        # avoid giving too much time to see the difference between dfs_backtrack and dfs_normal
+        scaled_time_limit = min(time_limit, requests_count * 2 - 1)
         # num of nodes needs to > 1
         # pick relatively big number of nodes to avoid long runtime of DFS-B
         scaled_nodes_count = max(nodes_count, requests_count // 3)
-        print(f"Nodes: {scaled_nodes_count}, Requests: {requests_count}")
+        print(f"Nodes: {scaled_nodes_count}, Requests: {requests_count}, Time: {scaled_time_limit}")
 
         graph = generate_requests_uniform(scaled_nodes_count, requests_count)
-        dfs_normal = find_lcf_outcome(graph, time_limit, no_removals=True)
-        dfs_backtrack = find_lcf_outcome(graph, time_limit, no_removals=False)
-        write_instance_to_file(i + 1, requests_count, nodes_count, time_limit, dfs_backtrack, dfs_normal)
+        dfs_normal = find_lcf_outcome(graph, scaled_time_limit, no_removals=True)
+        dfs_backtrack = find_lcf_outcome(graph, scaled_time_limit, no_removals=False)
+        write_instance_to_file(i + 1, requests_count, scaled_nodes_count, scaled_time_limit, dfs_backtrack, dfs_normal)
